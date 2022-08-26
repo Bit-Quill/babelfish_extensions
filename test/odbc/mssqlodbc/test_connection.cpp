e2e12cfcaba9e0a02dc1890fddd10f90fb6f33b1
Avneet Toor <avneett@bitquilltech.com>
2022-08-26 00:28:26 +0000

    Remove comments from test_connection test files

--- test/odbc/mssqlodbc/test_connection.cpp
+++ test/odbc/mssqlodbc/test_connection.cpp
@@ -28,32 +28,4 @@ TEST_F(MSSQL_Connection, SQLDriverConnect_1_SuccessfulConnectionTest) {
                                 SQL_DRIVER_COMPLETE);
 
   ASSERT_TRUE(rcode == SQL_SUCCESS_WITH_INFO || rcode == SQL_SUCCESS);
-  // SQLHENV env;
-  //   SQLHDBC dbc;
-  //   SQLHSTMT stmt;
-  //   SQLRETURN rc;
-
-  //   //*** Connecting to remote PostgreSQL database
-  //   rc = SQLAllocEnv( &env );
-  //   rc = SQLAllocConnect( env, &dbc );
-  //   if( SQL_SUCCEEDED(rc) )
-  //   {
-  //       std::cout << "allocation successful" << std::endl;
-  //   }
-  //   std::string _szDSN = "DRIVER={PostgreSQL ODBC Driver(UNICODE)};Server=localhost;Port=5432;Database=jdbc_testdb;UID=jdbc_user;PWD=12345678";
-  //   rc = SQLDriverConnect( dbc, NULL, (unsigned char*)(_szDSN.c_str()), SQL_NTS, NULL, 0, NULL, SQL_DRIVER_NOPROMPT );
-  //   if( SQL_SUCCEEDED(rc) )
-  //   {
-  //       std::cout << "Connection successful" << std::endl;
-  //   }
-  //   else
-  //   {
-  //       std::cout << "Connection UNsuccessful" << std::endl;
-  //   }
-
-
-  //   //*** Exit program
-  //   std::cout.flush();
-  //   std::getchar();
-  //   return;
 }
