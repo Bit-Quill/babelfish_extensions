788f3d48ed5303bf4176a2223188122396b56d40
Toby Cao <tobyc@bitquilltech.com>
2022-10-04 15:19:11 -0700

    rebase

--- test/odbc/psqlodbc/toby_test_pg_datatype_nvarchar.cpp
+++ test/odbc/psqlodbc/toby_test_pg_datatype_nvarchar.cpp
@@ -1,16 +1,17 @@
 #include <gtest/gtest.h>
 #include <sqlext.h>
-#include "odbc_handler.h"
-#include "query_generator.h"
+#include "../src/drivers.h"
+#include "../src/odbc_handler.h"
+#include "../src/query_generator.h"
 
 using std::pair;
 
-const string TABLE_NAME = "master_dbo.sysname_table_odbc_test";
-const string VIEW_NAME = "master_dbo.sysname_view_odbc_test";
-const string DATATYPE = "sys.SYSNAME";
+const string TABLE_NAME = "master_dbo.nvarchar_table_odbc_test";
+const string VIEW_NAME = "master_dbo.nvarchar_view_odbc_test";
+const string DATATYPE = "sys.NVARCHAR";
 const int NUM_COLS = 4;
-const string COL_NAMES[NUM_COLS] = {"pk", "sysname_1", "sysname_128", "sysname_20"  };
-const int COL_LENGTH[NUM_COLS] = {10, 1, 128, 20};
+const string COL_NAMES[NUM_COLS] = {"pk", "nvarchar_1", "nvarchar_4000", "nvarchar_20"};
+const int COL_LENGTH[NUM_COLS] = {10, 1, 4000, 20};
 
 const string COL_TYPES[NUM_COLS] = {
   DATATYPE + "(" + std::to_string(COL_LENGTH[0]) + ")",
@@ -19,37 +20,67 @@ const string COL_TYPES[NUM_COLS] = {
   DATATYPE + "(" + std::to_string(COL_LENGTH[3]) + ")"
 };
 
-vector<pair<string, string>> TABLE_COLUMNS_SYSNAME = {
-  {COL_NAMES[0], DATATYPE + " PRIMARY KEY"},
-  {COL_NAMES[1], DATATYPE},
-  {COL_NAMES[2], DATATYPE},
-  {COL_NAMES[3], DATATYPE}
+vector<pair<string, string>> TABLE_COLUMNS_NVARCHAR = {
+  {COL_NAMES[0], COL_TYPES[0] + " PRIMARY KEY"},
+  {COL_NAMES[1], COL_TYPES[1]},
+  {COL_NAMES[2], COL_TYPES[2]},
+  {COL_NAMES[3], COL_TYPES[3]}
 };
 
-const string STRING_128 = "TQR6vCl9UH5qg2UEJMleJaa3yToVaUbhhxQ7e0SgHjrKg1TYvyUzTrLlO64uPEj572WjgLK6X5muDjK64tcWBr4bBp8hjnV"
-  "ftzfLIYFEFCK0nAIuGhnjHIiB8Qc3ywbK";
+const string STRING_4000 = "TQR6vCl9UH5qg2UEJMleJaa3yToVaUbhhxQ7e0SgHjrKg1TYvyUzTrLlO64uPEj572WjgLK6X5muDjK64tcWBr4bBp8hjnV"
+  "ftzfLIYFEFCK0nAIuGhnjHIiB8Qc3ywbWqARbphlli11dyPOJi5KRTPMh1c5zMxyXEiyDhLxP5Hs96hIHOgV29e06vBaDJ2xSE2Pve32aX8EDLFDOial7t7ya2CluTP"
+  "lL0TMH8Tbcb58if64Hp4Db5clDdD3HdcFAGPJpkI69IhZHbJZXZLrYJ1iP00Nt63DPhEMOgrKeKzv4ByEjWKYkbCqAFqG5MyQ98QotkKgLwTYcn2x9Zv9IqKTFFBg5I"
+  "Yl3yQByGke2RMLjImDqYnMbkPzcAhSU25VuhM4sZ6K5vvOyPzIx1vmpbgX7lj8CDwFFIbMjghJLEIsqRBA2gQ6RXYLbsj62JqedNVrJiByhkrfSybsEG9o24UXT9MJZ"
+  "dthKPjIU7gDKSB6LIiO9axy9z93Xc7CuyxHHx9eHFXgHO9ZqyBwU24iUri9chQFVUGyPtHwSFtMqjtVBrzj04jXnc7WQPWKGzcbWBNnApQah1glDfXJoDGzNHcay7pD"
+  "E2OVgbF5uGVWDzXrj7o5ZLSLgwbRwgNfbb3TAwofa9Ju1XyRkvHBXPEUieYAR4Uvu1WAFHsvOlNqarmgECTon5zbSjYEA7oBhhZ4oimJyaHAE3xXHOcbOoComfzLdMz"
+  "hnLzoAQj4RHkfMQxx84ADJqIBOi3lW313NdFpEzDD2ZCiAynPVJHEbWAlllp4YRJIMe3cbPvIDw6C2uUHgifgwy1F61clEFzG2Z9QTM7mOBDBOldEjYeAoOLWPZlpyr"
+  "CNL6MgV2yj0MdIEFUzITJ68kAWXdgoCzD9McNwIYd3uBd9AE8Q3cOie2rX2opCeP8TErgj2dx3olonsEwxNqgUKj4u8wvfhydyjPbvc9SWog1R5AlrUWePptSFs1uJu"
+  "9bORoQCcsmKCvhoB0MGCGVERQCWIoaSuTwDZO0qfbnxZnj3D5bRFFLnuNhbz2KgxkP3B25nDOrlljsHcVpt07XWjNyel4Ju2s1QxdoJs9KAwNkWAWz66rDiDmQ6mIHKB"
+  "PUZ5r7PCnZtQCenTR93KdXmOMkM6JJAYbccLOgrw0j483AEau99adnT70C8vvvpdrLy4YRFuyYFNY5S2Rq2EQLZGrXjwDuJU3Ypieb73iHkfOre7XOFEvHL1La9Jb3dE"
+  "s9ekRA55OUCLlQVwDsGEtpzqGGDNLRNTa8EcPl3GwiHfK9gKt3bav6KszgFgOqiUO93mnp7IFInCWiiUxKZWC079CckDecff0bxzsqgimdkSUYavNDnvfvgBSEV3lPpK"
+  "dkADQbYUkmYzxvkkkMnMifJLvF1TrPb0bnoxQSYLUFUFjrm9HhYFElDNuMZWiKYSU9RJQbqjAffhZzpzFjk4oIStRfBrsLl4FakQgAUQdKxwAj3xGTxH4vmnXj83WRh0"
+  "EvI53AFxZWPUc0QtBnLSzeWI5Rvch7ZefNsq3uSDzDfQ8pFxGzaxQd0nLZWWM1G0HqvsWrLezPXIVXtgZ3DhS20kmA2cWFbnY1jMQ4ciXSVehIkbsY1zDLEHmi1NPDIf"
+  "O0LOw9IJGBEfvPbxOyzTsNcbU2op1ovJFIJ0zWXdaAyvKghrd9rpxoB7BktW5GuPspzzoqumapF3MIvTC8XYsWsp3JuO6UPpfrDczaDs2ikotg8k7UFnha4vAWzDZumq"
+  "lcUs9hiByF75R52udcx55wjBCYWqtvUvOYUU5mIw0wiPB0C1yFffzZtnNfZJbwHzXaMjztOx5lIq3EDFGxcFmMjJxHCd3WnvvOz5An9vZPwaiiYYQ0efqi6V4f0rHsY6"
+  "F9lNd1h9ns38HYG3EKCJfhHZx8e2Zg8Phy3ZPfwAo7Pq88mCBwmYlEcaR8gFoK0N7u44d6IPzlw3VBeKcXGfcMr07VL2wB9o3PJMUV9grlmhPUZ4yUyWKcrngoALyotg"
+  "AzmSrW8nwOsfL09pPBLV1zD0y3a8EtWhGvUMtz9PgdjZlPSDjvzszDyNqOQIvtKQvJfadS87ydQAA96NMAOVw0v9X22fAL4YnF5tkyz6ZglpojV2Y0ma0q0p2Lp5Uq88"
+  "l9wtMvCVgb2tA6SYz6VyBrxUws6wEhXYHdKDCMvLmyVLS9ifQg22qBsLczfrunWImEdxSRb0FclNXxhADvzxen9esm80VyvTmXvkPYMDV4SW9sXJN0ADSfbsjBuC5758W"
+  "Apte1bRfW02z111ZZxycSmF9TrvfwuhPrPEKDJfB4XSEIohCX6RDA8ccPxhkeCNNyal6X5qSXIk7S6WCJ3Tqcad270bxlflLLkXbUjX2MfQWdz9UGJQwO5tyPesP6Kt5k"
+  "WtiJoeBiltMX1lNTQjmLgidqMw6VNRiPwPXRgKiXCTV3nC506niztAnpeROYqDrQ0PfFajDtIEyUFKNan64rR65ySWUNtsooPfbptFGzRw21KL7UAjVgrk4hR6zuYDj5g"
+  "IdWIIKrNoiMUZpQ9tZhbkapLxFsZuRJXwl0aOvXGW3D539QP5KlwZPPX7LpPDLkYCNznfa5Nut2ol1hHy6KHBdr9r7kKg615vlMPUoriJpdrCefuDCa9cfywUjIWgCBdG"
+  "Jpt4uBYDAEQkOcmnW1NfPfXZWHIwk3RKZNj7Hp5o5Wx8KFdPZRDDDR9ztAmyojL7UDqsCdZkVDXz7EqDII47VVdxhmiFC0hHQdGuno1fW7VsxBVbCBHu0wowNXSul6PBV"
+  "Og3PntXRGKCpM5WcZrMPidDXRgOGI6GrVmxJLmozImWY7CVK5V6Nkfgi51hSsG9AyGPZNwg0hB35h2SQQoyrYvYzFxqc6hBFBu4KRMPlNz2tTDJor89aC7FVPWHXXIquP"
+  "fR1n2G72vqjQYv0OmVAJwtX6lckYh5okSBiZZOPaDIWf30qGi0JpiCs2MyNAVU9hfuoqYAVIt1W3PpqMjYBDfNse1xACP1YG80T6u9PH0CPnV2i9qLCclXi6I3ZX7rCTq"
+  "b7VnxLJ1KYS0wT8Fhh4jR1dXQq9SyvAMnQmx0Qm0ufJvTT1LC4nWN7OInFJXUtI4TY2fvpUm2pnXkTYtwMQVACX0oKsIVEkXuxrSB7ozA4n0q0ym751Pj4U8hNuRPv4ZP"
+  "ZXVW5gcQj7jtGfIvLHArPOJEPpyquljImYjSifCvfwxiHpRgjaHyYLQczOAVEs9ibflD1Chb7Ogd1I3UyqbQEgAOwzgXJTpeCfvrQI02Zqa43jc3Ah0fcSggpi2iil7XY"
+  "W658CbpzwT2rMNyPFuWWYtHsQibx3es4soysasVz8AMQw55XJsRCK7Fz8KRaMIVrpp8jt1dIs26eUGcV0TA1NxdTdF7ZlgVlaxqswtaQ2aMfvzVmkM0aAfiH0YB7JlJJa"
+  "1u0Z7UsQXlBAKkf9T2vNjUCe14geYyLMusk45KJvGVGJzZ12YGuZ8NRTVlaHDuEYsizKw6aZRBBjP5DCBZZznU2xsUerJnWt9rEooGTraF0X4tnU6JCZVpTF4WV2iDAm2"
+  "OgNCfo6qt5wOsXoAbtG4BxV6SjekfufQW0OSiIIEOvOuDcnYfa4q80ibBt642k6WVc6ExZT9y9KvlfNFzfmqQHk6OkhqQVSWkCfTTSCyC5bpskW2p8hPpZAISEI49GMVM"
+  "dKPMgxXq6XHYjdGnI5titO9a9Fw0ud1vGZDLV6PhlVl7Lelsn8WxRnHufoXvK2xNVXXuPd6sd5gqr90z3B1oV1sNRQRIIRfHSxAx1gmPMSuNWooRz9zVEYfDegrIKZFQb"
+  "7a5RbDg2OWxrWcX4KmfsgIozSpCGUoMv15WHuGeZrPvAmk3nyr7BrMhYqvMg13JceO82rER67IOxVXTM9KnVwlOxbmSnH1w3CzWrZVqzpKY5W0UPZB2tQXezqqMFHRhWG"
+  "L5KUxdTyPGBrTIyo1VesEBvkqgKzIiROBK6UVaP24WGl74nyGX5YGg9Cqs";
 
 const string STRING_1 = "a";
 const string STRING_20 = "0123456789abcdefghij";
 
-class PSQL_DataTypes_SYSNAME : public testing::Test{
+class PSQL_DataTypes_Nvarchar : public testing::Test{
 
   void SetUp() override {
 
-    OdbcHandler test_setup;
+    OdbcHandler test_setup(Drivers::GetDriver(ServerType::PSQL));
     test_setup.ConnectAndExecQuery(DropObjectStatement("TABLE", TABLE_NAME));
   }
 
   void TearDown() override {
 
-    OdbcHandler test_cleanup;
+    OdbcHandler test_cleanup(Drivers::GetDriver(ServerType::PSQL));
     test_cleanup.ConnectAndExecQuery(DropObjectStatement("VIEW", VIEW_NAME));
     test_cleanup.ExecQuery(DropObjectStatement("TABLE", TABLE_NAME));
   }
 };
 
 // helper function to initialize insert string (1, "", "", ""), etc.
-string InitializeInsertString(const vector<vector<string>> &inserted_values) {
+string InitializeInsertString_nvarchar(const vector<vector<string>> &inserted_values) {
 
   string insert_string{};
   string comma{};
@@ -73,16 +104,17 @@ string InitializeInsertString(const vector<vector<string>> &inserted_values) {
   return insert_string;
 }
 
-TEST_F(PSQL_DataTypes_SYSNAME, ColAttributes) {
+TEST_F(PSQL_DataTypes_Nvarchar, ColAttributes) {
 
-  const int LENGTH_EXPECTED = 128;
+  const int LENGTH_EXPECTED = 10;
   const int PRECISION_EXPECTED = 0;
   const int SCALE_EXPECTED = 0;
   const string NAME_EXPECTED = "unknown";
   const string PREFIX_EXPECTED = "'";
   const string SUFFIX_EXPECTED = "'";
+  const int BYTES_EXPECTED = 2;
 
-  const int BUFFER_SIZE = 128;
+  const int BUFFER_SIZE = 256;
   char name[BUFFER_SIZE];
   char suffix[BUFFER_SIZE];
   char prefix[BUFFER_SIZE];
@@ -93,10 +125,10 @@ TEST_F(PSQL_DataTypes_SYSNAME, ColAttributes) {
   SQLLEN is_case_sensitive;
 
   RETCODE rcode;
-  OdbcHandler odbcHandler;
+  OdbcHandler odbcHandler(Drivers::GetDriver(ServerType::PSQL));
 
   // Create a table with columns defined with the specific datatype being tested. 
-  odbcHandler.ConnectAndExecQuery(CreateTableStatement(TABLE_NAME, TABLE_COLUMNS_SYSNAME));
+  odbcHandler.ConnectAndExecQuery(CreateTableStatement(TABLE_NAME, TABLE_COLUMNS_NVARCHAR));
   odbcHandler.CloseStmt();
 
   // Select * From Table to ensure that it exists
@@ -113,8 +145,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, ColAttributes) {
                             NULL,
                             (SQLLEN*) &length);
     ASSERT_EQ(rcode, SQL_SUCCESS);
-    std::cout<<"The current column Length: "<< length<<'\n';
-    ASSERT_EQ(length, LENGTH_EXPECTED);
+    ASSERT_EQ(length, COL_LENGTH[i-1]);
     
     rcode = SQLColAttribute(odbcHandler.GetStatementHandle(),
                             i,
@@ -184,7 +215,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, ColAttributes) {
   odbcHandler.ExecQuery(DropObjectStatement("TABLE", TABLE_NAME));
 }
 
-TEST_F(PSQL_DataTypes_SYSNAME, Table_Create_Fail) {
+TEST_F(PSQL_DataTypes_Nvarchar, Table_Create_Fail) {
 
   vector<vector<pair<string, string>>> invalid_columns{
     {{"invalid1", DATATYPE + "(-1)"}},
@@ -194,7 +225,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, Table_Create_Fail) {
   };
 
   RETCODE rcode;
-  OdbcHandler odbcHandler;
+  OdbcHandler odbcHandler(Drivers::GetDriver(ServerType::PSQL));
 
   // Create a table with columns defined with the specific datatype being tested. 
   odbcHandler.Connect();
@@ -213,7 +244,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, Table_Create_Fail) {
   odbcHandler.ExecQuery(DropObjectStatement("TABLE", TABLE_NAME));
 }
 
-TEST_F(PSQL_DataTypes_SYSNAME, Insertion_Success) {
+TEST_F(PSQL_DataTypes_Nvarchar, Insertion_Success) {
 
   const int BUFFER_LENGTH = 8192;
 
@@ -222,11 +253,11 @@ TEST_F(PSQL_DataTypes_SYSNAME, Insertion_Success) {
   SQLLEN affected_rows;
 
   RETCODE rcode;
-  OdbcHandler odbcHandler;
+  OdbcHandler odbcHandler(Drivers::GetDriver(ServerType::PSQL));
 
   vector<vector<string>> inserted_values = {
     {"1", "", "", "" }, // empty strings
-    {"2", STRING_1, STRING_128, STRING_20}, // max values
+    {"2", STRING_1, STRING_4000, STRING_20}, // max values
     {"3", "a", "def", "ghi"}, // regular values
     {"4", "NULL", "NULL", "NULL"} // NULL values
   };
@@ -239,10 +270,10 @@ TEST_F(PSQL_DataTypes_SYSNAME, Insertion_Success) {
     bind_columns.push_back(tuple_to_insert);
   }
 
-  string insert_string = InitializeInsertString(inserted_values);
+  string insert_string = InitializeInsertString_nvarchar(inserted_values);
 
   // Create table
-  odbcHandler.ConnectAndExecQuery(CreateTableStatement(TABLE_NAME, TABLE_COLUMNS_SYSNAME));
+  odbcHandler.ConnectAndExecQuery(CreateTableStatement(TABLE_NAME, TABLE_COLUMNS_NVARCHAR));
   odbcHandler.CloseStmt();
 
   // Insert valid values into the table and assert affected rows
@@ -284,7 +315,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, Insertion_Success) {
   odbcHandler.ExecQuery(DropObjectStatement("TABLE", TABLE_NAME));
 }
 
-TEST_F(PSQL_DataTypes_SYSNAME, Insertion_Failure) {
+TEST_F(PSQL_DataTypes_Nvarchar, Insertion_Failure) {
 
   const int BUFFER_LENGTH = 8192;
 
@@ -292,14 +323,16 @@ TEST_F(PSQL_DataTypes_SYSNAME, Insertion_Failure) {
   SQLLEN col_len[NUM_COLS];
 
   RETCODE rcode;
-  OdbcHandler odbcHandler;
+  OdbcHandler odbcHandler(Drivers::GetDriver(ServerType::PSQL));
 
   vector<vector<string>> inserted_values = {
-    {"2", "", STRING_128 + "t", ""}, // second col exceeds by 1 char
+    {"1", STRING_1 + "1", "", "" }, // first col exceeds by 1 char
+    {"2", "", STRING_4000 + "1", ""}, // second col exceeds by 1 char
+    {"3", "", "", STRING_20 + "1"}, // third col exceeds by 1 char
   };
 
   // Create table
-  odbcHandler.ConnectAndExecQuery(CreateTableStatement(TABLE_NAME, TABLE_COLUMNS_SYSNAME));
+  odbcHandler.ConnectAndExecQuery(CreateTableStatement(TABLE_NAME, TABLE_COLUMNS_NVARCHAR));
   odbcHandler.CloseStmt();
 
   // Insert invalid values in table and assert error
@@ -329,7 +362,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, Insertion_Failure) {
   odbcHandler.ExecQuery(DropObjectStatement("TABLE", TABLE_NAME));
 }
 
-TEST_F(PSQL_DataTypes_SYSNAME, Update_Success) {
+TEST_F(PSQL_DataTypes_Nvarchar, Update_Success) {
 
   const int BUFFER_LENGTH = 8192;
   const int AFFECTED_ROWS_EXPECTED = 1;
@@ -340,7 +373,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, Update_Success) {
   SQLLEN affected_rows;
 
   RETCODE rcode;
-  OdbcHandler odbcHandler;
+  OdbcHandler odbcHandler(Drivers::GetDriver(ServerType::PSQL));
 
   vector<vector<string>> inserted_values = {
     {PK_VAL, "1", "2", "3"} 
@@ -348,7 +381,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, Update_Success) {
 
   vector<vector<string>> updated_values = {
     {PK_VAL, "a", "b", "c"}, // standard values
-    {PK_VAL, STRING_1, STRING_128, STRING_20}, // max values
+    {PK_VAL, STRING_1, STRING_4000, STRING_20}, // max values
     {PK_VAL, "", "", ""} // min values
   };
 
@@ -360,11 +393,11 @@ TEST_F(PSQL_DataTypes_SYSNAME, Update_Success) {
     bind_columns.push_back(tuple_to_insert);
   }
 
-  string insert_string = InitializeInsertString(inserted_values);
+  string insert_string = InitializeInsertString_nvarchar(inserted_values);
 
 
   // Create table
-  odbcHandler.ConnectAndExecQuery(CreateTableStatement(TABLE_NAME, TABLE_COLUMNS_SYSNAME));
+  odbcHandler.ConnectAndExecQuery(CreateTableStatement(TABLE_NAME, TABLE_COLUMNS_NVARCHAR));
   odbcHandler.CloseStmt();
 
   // Insert valid values into the table and assert affected rows
@@ -430,7 +463,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, Update_Success) {
   odbcHandler.ExecQuery(DropObjectStatement("TABLE", TABLE_NAME));
 }
 
-TEST_F(PSQL_DataTypes_SYSNAME, Update_Fail) {
+TEST_F(PSQL_DataTypes_Nvarchar, Update_Fail) {
 
   const int BUFFER_LENGTH = 8192;
   const int AFFECTED_ROWS_EXPECTED = 1;
@@ -441,14 +474,14 @@ TEST_F(PSQL_DataTypes_SYSNAME, Update_Fail) {
   SQLLEN affected_rows;
 
   RETCODE rcode;
-  OdbcHandler odbcHandler;
+  OdbcHandler odbcHandler(Drivers::GetDriver(ServerType::PSQL));
 
   vector<vector<string>> inserted_values = {
     {PK_VAL, "1", "2", "3"} 
   };
 
   vector<vector<string>> updated_values = {
-    {PK_VAL, STRING_1 + "1", STRING_128 + "1", STRING_20 + "1"} // max values + 1 char
+    {PK_VAL, STRING_1 + "1", STRING_4000 + "1", STRING_20 + "1"} // max values + 1 char
   };
 
   vector<tuple<int, int, SQLPOINTER, int, SQLLEN* >> bind_columns{};
@@ -459,10 +492,10 @@ TEST_F(PSQL_DataTypes_SYSNAME, Update_Fail) {
     bind_columns.push_back(tuple_to_insert);
   }
 
-  string insert_string = InitializeInsertString(inserted_values);
+  string insert_string = InitializeInsertString_nvarchar(inserted_values);
 
   // Create table
-  odbcHandler.ConnectAndExecQuery(CreateTableStatement(TABLE_NAME, TABLE_COLUMNS_SYSNAME));
+  odbcHandler.ConnectAndExecQuery(CreateTableStatement(TABLE_NAME, TABLE_COLUMNS_NVARCHAR));
   odbcHandler.CloseStmt();
 
   // Insert valid values into the table and assert affected rows
@@ -528,10 +561,9 @@ TEST_F(PSQL_DataTypes_SYSNAME, Update_Fail) {
   odbcHandler.ExecQuery(DropObjectStatement("TABLE", TABLE_NAME));
 }
 
-TEST_F(PSQL_DataTypes_SYSNAME, String_Operators) {
-  const int BUFFER_LENGTH=256;
+TEST_F(PSQL_DataTypes_Nvarchar, String_Operators) {
+  const int BUFFER_LENGTH=8192;
   const int BYTES_EXPECTED = 4;
-  const int DOUBLE_BYTES_EXPECTE = 8;
   int pk;
   char data[BUFFER_LENGTH];
   SQLLEN pk_len;
@@ -539,11 +571,11 @@ TEST_F(PSQL_DataTypes_SYSNAME, String_Operators) {
   SQLLEN affected_rows;
 
   RETCODE rcode;
-  OdbcHandler odbcHandler;
+  OdbcHandler odbcHandler(Drivers::GetDriver(ServerType::PSQL));
 
 const int NUM_COLS = 2;
 const string COL_NAMES[NUM_COLS] = {"pk", "data"};
-const int COL_LENGTH[NUM_COLS] = {128, 128};
+const int COL_LENGTH[NUM_COLS] = {8190, 8190};
 
 const string COL_TYPES[NUM_COLS] = {
   DATATYPE,
@@ -641,7 +673,8 @@ vector<pair<string, string>> TABLE_COLUMNS_NTEXT = {
 }
 
 
-TEST_F(PSQL_DataTypes_SYSNAME, View_creation) {
+
+TEST_F(PSQL_DataTypes_Nvarchar, View_creation) {
 
   const string VIEW_QUERY = "SELECT * FROM " + TABLE_NAME;
   const int BUFFER_LENGTH = 8192;
@@ -651,11 +684,11 @@ TEST_F(PSQL_DataTypes_SYSNAME, View_creation) {
   SQLLEN affected_rows;
 
   RETCODE rcode;
-  OdbcHandler odbcHandler;
+  OdbcHandler odbcHandler(Drivers::GetDriver(ServerType::PSQL));
 
   vector<vector<string>> inserted_values = {
     {"1", "", "", "" }, // empty strings
-    {"2", STRING_1, STRING_128, STRING_20}, // max values
+    {"2", STRING_1, STRING_4000, STRING_20}, // max values
     {"3", "a", "def", "ghi"}, // regular values
     {"4", "NULL", "NULL", "NULL"} // NULL values
   };
@@ -668,10 +701,10 @@ TEST_F(PSQL_DataTypes_SYSNAME, View_creation) {
     bind_columns.push_back(tuple_to_insert);
   }
 
-  string insert_string = InitializeInsertString(inserted_values);
+  string insert_string = InitializeInsertString_nvarchar(inserted_values);
 
   // Create table
-  odbcHandler.ConnectAndExecQuery(CreateTableStatement(TABLE_NAME, TABLE_COLUMNS_SYSNAME));
+  odbcHandler.ConnectAndExecQuery(CreateTableStatement(TABLE_NAME, TABLE_COLUMNS_NVARCHAR));
   odbcHandler.CloseStmt();
 
   // Insert valid values into the table and assert affected rows
@@ -719,7 +752,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, View_creation) {
   odbcHandler.ExecQuery(DropObjectStatement("VIEW", VIEW_NAME));
 }
 
-TEST_F(PSQL_DataTypes_SYSNAME, Table_Composite_Keys) {
+TEST_F(PSQL_DataTypes_Nvarchar, Table_Composite_Keys) {
   const vector<pair<string, string>> TABLE_COLUMNS = {
     {COL_NAMES[0], "int" },
     {COL_NAMES[1], DATATYPE}
@@ -741,7 +774,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, Table_Composite_Keys) {
   table_constraints += ")";
 
   const int PK_BYTES_EXPECTED = 4;
-  const int BUFFER_LENGTH=256;
+  const int BUFFER_LENGTH=8192;
   int pk;
   char data[BUFFER_LENGTH];
   SQLLEN pk_len;
@@ -749,7 +782,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, Table_Composite_Keys) {
   SQLLEN affected_rows;
 
   RETCODE rcode;
-  OdbcHandler odbcHandler;
+  OdbcHandler odbcHandler(Drivers::GetDriver(ServerType::PSQL));
 
   const vector<string> VALID_INSERTED_VALUES = {
     STRING_1,
@@ -853,8 +886,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, Table_Composite_Keys) {
   odbcHandler.ExecQuery(DropObjectStatement("TABLE", TABLE_NAME));
 }
 
-
-TEST_F(PSQL_DataTypes_SYSNAME, Table_Unique_Constraints) {
+TEST_F(PSQL_DataTypes_Nvarchar, Table_Unique_Constraints) {
   const vector<pair<string, string>> TABLE_COLUMNS = {
     {COL_NAMES[0], "INT PRIMARY KEY" },
     {COL_NAMES[1], DATATYPE + " UNIQUE"}
@@ -864,7 +896,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, Table_Unique_Constraints) {
   const string UNIQUE_COLUMN_NAME = COL_NAMES[1];
 
   const int PK_BYTES_EXPECTED = 4;
-  const int BUFFER_LENGTH=256;
+  const int BUFFER_LENGTH=8192;
   int pk;
   char data[BUFFER_LENGTH];
   SQLLEN pk_len;
@@ -872,7 +904,7 @@ TEST_F(PSQL_DataTypes_SYSNAME, Table_Unique_Constraints) {
   SQLLEN affected_rows;
 
   RETCODE rcode;
-  OdbcHandler odbcHandler;
+  OdbcHandler odbcHandler(Drivers::GetDriver(ServerType::PSQL));
 
   const vector<string> VALID_INSERTED_VALUES = {
     "0",
